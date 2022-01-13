601cb111decfa4673f9509a4da8f5379b3f20b3b
niyi28 <61952240+niyi28@users.noreply.github.com>
2022-01-13 19:23:34 +0100

    initial build of birthday app

---
+++ app/src/main/java/com/example/birthdayApp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.secondbirthday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val num: Int = 3
+        setContentView(R.layout.activity_main)
+        
+
+    }
+}
